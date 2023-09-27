8d48253b1679944cf5d88171905423e824d9af81
natthawe <natthawejumjai@gmail.com>
2023-09-27 12:10:35 +0700

    update

--- src/pointcloud_to_laserscan/launch/omron2laser_back_launch.py
+++ src/pointcloud_to_laserscan/launch/omron2laser_back_launch.py
@@ -11,14 +11,14 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': 'back_laser',
                 'transform_tolerance': 0.01,
-                'min_height': -0.2,
+                'min_height': -0.18,
                 'max_height': 0.3,
-                'angle_min': -1.5708, #-0.75921822462, # -M_PI/2
-                'angle_max': 1.5708, #0.75921822462, # M_PI/2
+                'angle_min': -0.7679448709, #-90 =-1.5708, #43.5 = -0.75921822462, # -M_PI/2
+                'angle_max': 0.7679448709,  # 90 = 1.5708, #43.5 = -0.75921822462, # M_PI/2
                 'angle_increment': 0.0087, #0.01745329238474369,  # M_PI/360.0
                 'scan_time': 0.3333,
                 'range_min': 0.5,
-                'range_max': 2.8,
+                'range_max': 3.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
